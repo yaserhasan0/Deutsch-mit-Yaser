0423133e04962372f5609efb4f90273774589460
yaserhasan0 <ing.yaserhasan@gmail.com>
2025-12-25 16:06:17 +0100

    Enable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/Deutsch-mit-Yaser/', // تأكد من مطابقة اسم المستودع
-  plugins: [react()],
-  build: {
-    minify: 'terser', // سنكتفي بهذا النوع من الحماية حالياً
-  }
+  base: '/Deutsch-mit-Yaser/', 
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Deutsch mit Yaser',
+        short_name: 'DeutschYaser',
+        description: 'Your companion for German grammar',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // تأكد من وجود هذه الصور في مجلد public
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
